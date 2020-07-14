5688610fca6c051f9783cce0f9a001a7f79428d7
sargis719 <sargishovhan@gmail.com>
2020-07-14 11:17:52 -0700

    added try/except to my project and fixed new game feature (don't need to ask players' names and marks every time they start a new game)

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,19 @@
 import random
 
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+class Not_Available(Error):
+    """Raised when the given position on the board is not available"""
+    pass
+class Out_Of_Range(Error):
+    """Raised when the input is out of a certain range"""
+    pass
+
+class CustomException(Error):
+    """Custom made Exception for handling multiple type of exceptions"""
+    pass
+
 def print_board(brd):
     print(brd[1] + '|' + brd[2] + '|' + brd[3])
     print(brd[4] + '|' + brd[5] + '|' + brd[6])
@@ -10,12 +24,18 @@ def play_again():
     This function asks the player if they want to play again
     and returns a boolean True if they do want to play again.
     """
-    answer = input("Do you want to play again y/n? ")
-    while answer != 'y' and answer != 'Y' and answer != 'n' and answer != 'N':
-        answer = input("Please enter Y or N ")
+    while True:
+        try:
+            answer = input("Do you want to play again y/n? ")
+            if answer != 'y' and answer != 'Y' and answer != 'n' and answer != 'N':
+                raise CustomException
+            break
+        except CustomException:
+            print("Please Enter Y or N")
     if answer == 'y' or answer == 'Y':
         return True
-    return False
+    else:
+        return False
 
 def is_available(brd1, aspace):
     """
@@ -32,13 +52,21 @@ def next_step(brd2, name):
     and then uses is_available to check if its a free position.
     If it is, then return the position for later use.
     """
-    next_pos = 0
-    while next_pos < 1 or next_pos > 9:
-        next_pos = int(input("{} please enter your position (number 1-9): ".format(name)))
-        if 1 <= next_pos <= 9:
-            if is_available(brd2, next_pos) == False:
-                print("The position is not available!")
-                next_pos = 0
+    while True:
+        try:
+            next_pos = int(input("{} please enter your position (number 1-9): ".format(name)))
+            if next_pos < 1 or next_pos > 9:
+                raise Out_Of_Range
+            else:
+                if is_available(brd2, next_pos) == False:
+                    raise Not_Available
+            break
+        except ValueError:
+            print("Not a valid entry!")
+        except Out_Of_Range:
+            print("Please enter a number between 1-9")
+        except Not_Available:
+            print("The position is not available!")
     return next_pos
 
 def is_full(brd3):
@@ -64,11 +92,14 @@ def user_marker():
     """
     This function takes in a player input and assign their marker as 'X' or 'O'.
     """
-    marker = input("Please select 'x' or 'o': ")
-    while marker != 'x':
-        if marker == 'o':
+    while True:
+        try:
+            marker = input("Please select 'x' or 'o': ")
+            if marker != 'x' and marker != 'X' and marker != 'o' and marker != 'O':
+                raise CustomException
             break
-        marker = input("Please select 'x' or 'o':")
+        except CustomException:
+            print("Invalid Entry!")
     return marker
 
 def board_assign(brd4, mark, pos):
@@ -104,30 +135,32 @@ def is_winner(my_board):
 
 print("Hello, let's play Tic Tac Toe")
 new_game = True
+player1 = {"name": " ", "mark": " ", "first": False, "winner": False}
+player2 = {"name": " ", "mark": " ", "first": False, "winner": False}
+player1["name"] = input("Please enter first player name: ")
+player2["name"] = input("Please enter second player name: ")
+if who_goes_first() == 1:
+    player1["first"] = True
+    print(player1["name"], " starts the game")
+    mark = user_marker()
+    if mark == 'x' or mark == 'X':
+        player1["mark"] = 'x'
+        player2["mark"] = 'o'
+    else:
+        player1["mark"] = 'o'
+        player2["mark"] = 'x'
+else:
+    player2["first"] = True
+    print(player2["name"], " starts the game")
+    mark = user_marker()
+    if mark == 'x' or mark == 'X':
+        player2["mark"] = 'x'
+        player1["mark"] = 'o'
+    else:
+        player2["mark"] = 'o'
+        player1["mark"] = 'x'
 while new_game:
     board = ['*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    player1 = {"name": " ", "mark": " ", "first": False}
-    player2 = {"name": " ", "mark": " ", "first": False}
-    player1["name"] = input("Please enter first player name: ")
-    player2["name"] = input("Please enter second player name: ")
-    if who_goes_first() == 1:
-        player1["first"] = True
-        print(player1["name"], " starts the game")
-        if user_marker() == 'x':
-            player1["mark"] = 'x'
-            player2["mark"] = 'o'
-        else:
-            player1["mark"] = 'o'
-            player2["mark"] = 'x'
-    else:
-        player2["first"] = True
-        print(player2["name"], " starts the game")
-        if user_marker() == 'x':
-            player2["mark"] = 'x'
-            player1["mark"] = 'o'
-        else:
-            player2["mark"] = 'o'
-            player1["mark"] = 'x'
     while is_full(board) == False and is_winner(board) == False:
         if player1["first"] == True:
             board_assign(board, player1["mark"], next_step(board, player1["name"]))
@@ -148,16 +181,32 @@ while new_game:
     if is_winner(board):
         if is_winner(board)[1] == 'x':
             if player1["mark"] == 'x':
+                player1["winner"] = True
+                player2["winner"] = False
                 print("Congratulations {}, you won!".format(player1["name"]))
             else:
+                player2["winner"] = True
+                player1["winner"] = False
                 print("Congratulations {}, you won!".format(player2["name"]))
         else:
             if player1["mark"] == 'o':
+                player1["winner"] = True
+                player2["winner"] = False
                 print("Congratulations {}, you won!".format(player1["name"]))
             else:
+                player2["winner"] = True
+                player1["winner"] = False
                 print("Congratulations {}, you won!".format(player2["name"]))
     if play_again():
         new_game = True
+        if player1["winner"] == True:
+            player1["first"] = True
+            player2["first"] = False
+            print(player1["name"], " starts the game")
+        else:
+            player2["first"] = True
+            player1["first"] = False
+            print(player2["name"], " starts the game")
     else:
         print("Thank you for playing. Bye!")
         new_game = False
